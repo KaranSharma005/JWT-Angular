4fa0c5e8571d8f0ccc81e9adab456e51b382e48a
karan <karan@DESKTOP-8CTNE0H>
2025-10-03 17:54:28 +0530

    first commit

--- AngularProject1/src/app/app.routes.ts
+++ AngularProject1/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Register } from './Components/register/register';
+import { Login } from './Components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"register", component : Register
+    },
+    {
+        path:"login", component : Login
+    }
+];
